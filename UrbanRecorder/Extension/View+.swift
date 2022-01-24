944f70d1556668881103d4749391ce999c1cdbc9
ting hui wu <>
2022-01-24 16:31:39 +0800

    1. Remove deprecated method

--- UrbanRecorder/Extension/View+.swift
+++ UrbanRecorder/Extension/View+.swift
@@ -108,8 +108,7 @@ struct KeyboardResponsiveModifier: ViewModifier {
                 NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notif in
                     let value = notif.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
                     let height = value.height
-                    let bottomInset = UIApplication.shared.windows.first?.safeAreaInsets.bottom
-                    self.offset = height - (bottomInset ?? 0)
+                    self.offset = height
                 }
                 
                 NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { notif in
