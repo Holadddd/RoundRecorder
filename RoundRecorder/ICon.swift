3a6e159e67487903a90385b22f7ddbda6ab5c139
ting hui wu <>
2022-01-25 18:04:25 +0800

    1. AppIcon Added

---
+++ RoundRecorder/ICon.swift
@@ -0,0 +1,84 @@
+//
+//  ICon.swift
+//  RoundRecorder
+//
+//  Created by ting hui wu on 2022/1/25.
+//
+
+import SwiftUI
+import Neumorphic
+
+struct ICon: View {
+    
+    var body: some View {
+        ZStack{
+            Rectangle().fill(Color.Neumorphic.main)
+            
+            ZStack{
+                Path { path in
+                    let width = UIScreen.main.bounds.width
+                    let height = UIScreen.main.bounds.height
+                    
+                    let x1: CGFloat = width * 0.62
+                    let y1 = height - 460
+                    
+                    let y2 = y1 - 84
+                    // 2.
+                    path.addLines( [
+                        CGPoint(x: width - x1, y: y2),
+                        CGPoint(x: width - x1, y:  y1 - 130)
+                        
+                    ])
+                    
+                    let x3: CGFloat = width * 0.385
+                    
+                    let x5: CGFloat = width * 0.33
+                    // 3.)
+                    path.addLines( [
+                        CGPoint(x: width - x3, y: y1 - 125),
+                        CGPoint(x: width - x5, y:  y2)
+                    ])
+                    // 4. Circle
+                    let cx1: CGFloat = width/2 - 100
+                    let cy1: CGFloat = width/2 - 130
+                    
+                    path.addEllipse(in: CoreGraphics.CGRect(x: cx1, y: cy1, width: 200, height: 200))
+                    
+                    // 5.
+                    path.closeSubpath()
+                }.strokedPath(StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(.red)
+                    .softOuterShadow()
+                
+                Path { path in
+                    let width = UIScreen.main.bounds.width
+                    let height = UIScreen.main.bounds.height
+                    
+                    // 4. Circle
+                    let cx1: CGFloat = width/2 - 30
+                    let cy1: CGFloat = width/2 - 60
+                    
+                    path.addEllipse(in: CoreGraphics.CGRect(x: cx1, y: cy1, width: 60, height: 60))
+                    
+                    // 5.
+                    path.closeSubpath()
+                }.fill(.red)
+                    .softOuterShadow()
+            }.offset(x: 0, y: 10)
+            
+        }.aspectRatio(1, contentMode: .fit)
+            .frame(width:  UIScreen.main.bounds.width, height:  UIScreen.main.bounds.width)
+        
+        
+    }
+}
+
+struct ICon_Previews: PreviewProvider {
+    static var previews: some View {
+        ICon()
+            .environment(\.colorScheme, .light)
+        
+        ICon()
+            .environment(\.colorScheme, .dark)
+    }
+}
